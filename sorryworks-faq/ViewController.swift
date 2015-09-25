84ecbc699b7253a127daa3f7899aa998d15034cf
Todd Crone <todd.crone@perceptivesoftware.com>
2015-09-25 01:42:16 -0400

    Add call to get faqs on load

--- sorryworks-faq/ViewController.swift
+++ sorryworks-faq/ViewController.swift
@@ -12,7 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            let request = NSMutableURLRequest(URL: NSURL(string: "https://sorryworks-faq.herokuapp.com/faq")!)
+        
+            request.HTTPMethod = "GET"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("EAD3757", forHTTPHeaderField: "x-catalog-token")
+        
+            let session = NSURLSession.sharedSession()
+        
+            let task = session.dataTaskWithRequest(request) {(data, response, error) in
+                print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+            }
+        
+            task.resume()
     }
 
     override func didReceiveMemoryWarning() {
