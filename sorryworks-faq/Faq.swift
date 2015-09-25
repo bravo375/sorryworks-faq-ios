806a7b03eae893598737ff63b1ee51c76d14ab31
Todd Crone <todd.crone@perceptivesoftware.com>
2015-09-25 05:00:01 -0400

    Fix table view load of data

---
+++ sorryworks-faq/Faq.swift
@@ -0,0 +1,18 @@
+//
+//  Faq.swift
+//  sorryworks-faq
+//
+//  Created by Todd Crone on 9/25/15.
+//  Copyright © 2015 Bravo375. All rights reserved.
+//
+
+import UIKit
+
+class Faq: NSObject {
+    var question: String?
+    var answer: NSURL?
+    
+    override init() {
+        super.init()
+    }
+}
--- sorryworks-faq/ViewController.swift
+++ sorryworks-faq/ViewController.swift
@@ -10,85 +10,84 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var faqs: Array <Faq> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            let request = NSMutableURLRequest(URL: NSURL(string: "https://sorryworks-faq.herokuapp.com/faq")!)
-        
-            request.HTTPMethod = "GET"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue("EAD3757", forHTTPHeaderField: "x-catalog-token")
-        
-            let session = NSURLSession.sharedSession()
-        
-            let task = session.dataTaskWithRequest(request) {(data, response, error) in
-                print("Response: \(response)")
-                let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                print("Body: \(strData)")
-                do {
-                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableLeaves) as? NSDictionary
-                    if let parseJSON = json {
-                        // Okay, the parsedJSON is here, let's get the value for 'success' out of it
-                        print("Succes: \(parseJSON)")
-                    }
-                    else {
-                        // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-                        let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                        print("Error could not parse JSON: \(jsonStr)")
-                    }
-                }
-                catch {
-                        print("Failed parse")
-                    }
-                }
-            task.resume()
+        reloadFaqs()
+
+        let refresher = UIRefreshControl()
+        refresher.addTarget(self, action: "handleRefresh:",
+            forControlEvents: UIControlEvents.ValueChanged)
+        refreshControl = refresher
     }
 
+    @IBAction func handleRefresh(sender : AnyObject) {
+        reloadFaqs()
+        refreshControl!.endRefreshing()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 5
+
+    func reloadFaqs() {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://sorryworks-faq.herokuapp.com/faq")!)
+        
+        request.HTTPMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("EAD3757", forHTTPHeaderField: "x-catalog-token")
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request) {(data, response, error) in
+            print("Response: \(response)")
+            let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("Body: \(strData)")
+            do {
+                let json: AnyObject? = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions(rawValue: 0))
+                self.faqs.removeAll(keepCapacity: true)
+                if let jsonArray = json as? [[String:AnyObject]] {
+                    for faqDict in jsonArray {
+                        let faq = Faq()
+                        faq.question = faqDict["question"] as? String
+                        faq.answer = NSURL(string: faqDict["answer"] as! String)!
+                        print(faq.question)
+                        self.faqs.append(faq)
+                        print(self.faqs.count)
+                    }
+                    print("Reloading data")
+                    self.tableView.reloadData()
+                    print("Done reloading data")
+                }
+                    else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    print("Error could not parse JSON: \(jsonStr)")
+                }
+            }
+                catch {
+                    print("Failed parse")
+            }
+        }
+        task.resume()
     }
-    
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section + 1
+        print("Number of rows \(faqs.count)")
+        return faqs.count
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView (_tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-        cell.textLabel!.text = "Row \(indexPath.row)"
+        let faq = faqs[indexPath.row]
+        cell.textLabel?.text = faq.question
         return cell
     }
-    
-//    import UIKit
-//    import XCPlayground
-//    
-//    XCPSetExecutionShouldContinueIndefinitely()
-//    
-//    var request = NSMutableURLRequest(URL: NSURL(string: "https://sorryworks-faq.herokuapp.com/faq")!)
-//    
-//    request.HTTPMethod = "GET"
-//    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//    request.addValue("application/json", forHTTPHeaderField: "Accept")
-//    request.addValue("EAD3757", forHTTPHeaderField: "x-catalog-token")
-//    
-//    let session = NSURLSession.sharedSession()
-//    
-//    let task = session.dataTaskWithRequest(request) {(data, response, error) in
-//        print(NSString(data: data!, encoding: NSUTF8StringEncoding))
-//    }
-//    
-//    //let task = session.dataTaskWithURL(url!) {(data, response, error) in
-//    //    print(NSString(data: data!, encoding: NSUTF8StringEncoding))
-//    //}
-//    
-//    task.resume()
 }
-
